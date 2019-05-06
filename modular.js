9a655f2a94bcf98d2771f8f48c2f3cee40861fe0
Brian Murray <brianmurraysound@gmail.com>
2019-05-06 15:09:23 +0000

    pass an object into callback function

---
+++ modular.js
@@ -0,0 +1,22 @@
+var https = require('https');
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML (options) {
+
+  https.get(options, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log("Combined data ", data);
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+  });
+}
+getAndPrintHTML(requestOptions);
